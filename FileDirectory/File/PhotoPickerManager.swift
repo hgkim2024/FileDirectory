5c48e45e2d13cfb68543eba7481c5ba1af689956
김현구 <auskim20@gmail.com>
2024-03-28 14:53:59 +0900

    Photo Picker 에서 Photo 선택하여 iv 에 표기하는 기능 개발

---
+++ FileDirectory/File/PhotoPickerManager.swift
@@ -0,0 +1,92 @@
+//
+//  FileManager.swift
+//  FileDirectory
+//
+//  Created by 김현구 on 3/28/24.
+//
+
+import Foundation
+import Photos
+import PhotosUI
+
+//struct UploadFileData {
+//    init(data: Data, path: URL, conversationID: String? = nil) {
+//        self.data = data
+//        self.path = path
+//        self.conversationID = conversationID
+//    }
+//    
+//    let data: Data
+//    let path: URL
+//    let conversationID: String?
+//}
+
+class PhotoPickerManager {
+    static let sharedInstance = PhotoPickerManager()
+    private init() { }
+    
+    private func photoAuth(_ completion: (() -> Void)? = nil) {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite, handler: { (status) in
+            switch status {
+            case .notDetermined:
+                break
+            case .restricted:
+                NSLog("[@Photo] Photo Authorization - restricted")
+                break
+            case .denied:
+                NSLog("[@Photo] Photo Authorization - denied")
+                break
+            case .authorized:
+                NSLog("[@Photo] Photo Authorization - authorized")
+                DispatchQueue.main.async {
+                    completion?()
+                }
+                break
+            case .limited:
+                NSLog("[@Photo] Photo Authorization - limited")
+                break
+            default:
+                break
+            }
+        })
+    }
+    
+    func showPhotoPicker(vc: ViewController) {
+        photoAuth() {
+            var config = PHPickerConfiguration(photoLibrary: .shared())
+            config.preferredAssetRepresentationMode = .current
+            config.selection = .ordered
+            config.selectionLimit = 1
+//            let newFilter = PHPickerFilter.any(of: [.images, .videos, .livePhotos, .])
+//            config.filter = newFilter
+            
+            let picker = PHPickerViewController(configuration: config)
+            picker.delegate = vc
+            
+            vc.present(picker, animated: true)
+        }
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult], _ completion: ((CGImage?) -> Void)? = nil) {
+        guard let result = results.first else {
+            picker.dismiss(animated: true)
+            completion?(nil)
+            return
+        }
+        
+        let itemProvider = result.itemProvider
+        if itemProvider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
+            itemProvider.loadDataRepresentation(forTypeIdentifier: UTType.image.identifier) { data, error in
+                guard let data = data,
+                      let cgImageSource = CGImageSourceCreateWithData(data as CFData, nil),
+                      let properties = CGImageSourceCopyPropertiesAtIndex(cgImageSource, 0, nil) else { return }
+                
+                let image = CGImageSourceCreateImageAtIndex(cgImageSource, 0, properties)!
+                DispatchQueue.main.async {
+                    completion?(image)
+                    picker.dismiss(animated: true)
+                }
+            }
+        }
+    }
+}
--- FileDirectory/ViewController.swift
+++ FileDirectory/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet var selectedImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onClickPhoto(_ sender: Any) {
+        PhotoPickerManager.sharedInstance.showPhotoPicker(vc: self)
+    }
+}
+
+// MARK: - PHPickerViewControllerDelegate
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        PhotoPickerManager.sharedInstance.picker(picker, didFinishPicking: results) { cgImage in
+            if let cgImage = cgImage {
+                self.selectedImage.image = UIImage(cgImage: cgImage)
+            } else {
+                // : not sected photo
+            }
+        }
+    }
 }
 
